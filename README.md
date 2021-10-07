03a1203a27badf188de8e20ed9ece73db386f9dc
CR Ewert <craig@eeewert.org>
2021-10-07 15:59:27 -0600

    ADDED signal to catch and handle interrupts, and goroutine to loop until signalled

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # go primes
+
+
+## This is a program that generates primes
+
+We dont need to generate primes, but its a tool to learn go,
+And, maybe, a tool for BDD, or Testing, or other things.
+
+v1: the simplest. Start at 3, divide by every number possible
+
+v2: add signals so I can catch an interrupt, and goroutine, so I can loop until I do.
--- primes.go
+++ primes.go
@@ -1,22 +1,60 @@
 package main
-import "fmt"
+
+import (
+    "fmt"
+    "os"
+    "os/signal"
+    "syscall"
+)
+
 func main() {
   fmt.Println("I will now generate primes forever")
   fmt.Printf("%d ",2) // the first prime, taken as given
   candidate := 3 // the next possible prime
 
-  for true {
-    isprime:=true // until shown otherwise
-    for ii:=2;ii<candidate;ii++ {
-      if candidate % ii == 0 {
-        isprime=false
-        break // from the for loop?
+  sigs := make(chan os.Signal, 1)
+  signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+  done := make(chan bool,1)
+
+  go func() {
+    for {
+      select {
+      case <- done:
+        fmt.Printf("\ngot a done so exiting\n")
+        done <- false
+        return
+      default:
+        isprime:=true // until shown otherwise
+        for ii:=2;ii<candidate;ii++ {
+          select {
+          case <-done:
+            fmt.Printf("\ngot done true in inner loop. not even finishing this check of %v\n",candidate)
+            done <- true
+            return
+          default:
+          }
+          if candidate % ii == 0 {
+            isprime=false
+            break
+          }
+        }  
+        if isprime {
+          fmt.Printf("%d ",candidate)
+        }
+        candidate+=1
       }
-    }  
-    if isprime {
-      fmt.Printf("%d ",candidate)
     }
-    candidate+=1
-  }
+  }()
+
+
+  sig := <- sigs
+  fmt.Printf("\n\ninterrupting because %v\n\n",sig)
+  done <- true
+
+  inner := <- done
+  fmt.Printf("\nDone acked %v, so can store state and stuff here.\n",inner)
+
+  fmt.Println("Really done")
+
 }
 
