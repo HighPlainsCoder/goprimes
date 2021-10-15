9b04af3286cc33a49eb99fc5a385faac8e5e6f27
CR Ewert <craig@eeewert.org>
2021-10-15 15:48:39 -0600

    MOVED code into functions, ignore the ide directory

--- README.md
+++ README.md
@@ -5,10 +5,24 @@
 
 We dont need to generate primes, but its a tool to learn go,
 And, maybe, a tool for BDD, or Testing, or other things.
+                                           
+### Evolution
 
 v1: the simplest. Start at 3, divide by every number possible
 
 v2: add signals so I can catch an interrupt, and goroutine, so I can loop until I do.
 
-v2.1: cleaned up the signalling; refactored how the select is coded; added proper wait for shutdown
+v2.1: cleaned up the signalling; refactored how the select is coded; added proper wait for shutdown.
+I also added some sleeps around, so the program runs slowly enough to watch with my old human eyes.
+                             
+v2.9: I pulled the code out into functions, and started the first tiny improvement to getNextCandidate,
+which only needs to check the odd numbers after 2.  I also moved it into Intj, where it works fine.
 
+                                                                                                
+## The go features used
+* goroutines
+* channels
+* signal
+* time
+* fmt
+* global variables
--- primes.go
+++ primes.go
@@ -9,50 +9,65 @@ import (
     "time"
 )
 
+var candidate int = 3 // the next possible prime
+
+func getNextCandidate() {
+  candidate += 2
+}
+
+func testPossible(applicant int, done chan struct{}) bool {
+  isprime:=true // until shown otherwise
+  for ii:=2;ii<applicant;ii++ {
+    time.Sleep(1*time.Second)
+
+    select {
+    case <-done:
+      fmt.Printf("\ngot done in inner loop. not even finishing this check of %v\n",applicant)
+      time.Sleep(1*time.Second)
+      return false
+    default:
+    }
+
+    if applicant % ii == 0 {
+      isprime=false
+      break
+    }
+  }
+  return isprime
+}
+
+func looper(done chan struct{}) {
+  for {
+    // this gag keeps showing up where channels & goroutines are
+    select {
+    case <- done:
+      fmt.Printf("\ngot a done so exiting\n")
+      return
+    default:
+    }
+
+    time.Sleep(1*time.Second)
+    isprime := testPossible(candidate, done)
+    if isprime {
+      fmt.Printf("%d ",candidate)
+    }
+
+    getNextCandidate()
+  }
+}
+
+
+
+
 func main() {
   fmt.Println("I will now generate primes forever")
   fmt.Printf("%d ",2) // the first prime, taken as given
-  candidate := 3 // the next possible prime
 
   sigs := make(chan os.Signal, 1)
   signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
   done := make(chan struct{},1)
 
-  go func() {
-    for {
-      // this gag keeps showing up where channels & goroutines are
-      select {
-      case <- done:
-        fmt.Printf("\ngot a done so exiting\n")
-        return
-      default:
-      }
-
-      time.Sleep(1*time.Second)
-      isprime:=true // until shown otherwise
-      for ii:=2;ii<candidate;ii++ {
-        time.Sleep(1*time.Second)
-        
-        select {
-        case <-done:
-          fmt.Printf("\ngot done in inner loop. not even finishing this check of %v\n",candidate)
-          time.Sleep(1*time.Second)
-          return
-        default:
-        }
-
-        if candidate % ii == 0 {
-          isprime=false
-          break
-        }
-      }  
-      if isprime {
-        fmt.Printf("%d ",candidate)
-      }
-      candidate+=1
-    }
-  }()
-
+  go looper(done)
 
   <- sigs
   fmt.Printf("\n\ninterrupted by user ...\n\n")
